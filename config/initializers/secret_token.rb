54f0a29fc594d4ba9e13119d143ec0407b22950d
Alex Yang <youremail@gmail.com>
2013-08-28 13:17:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abu::Application.config.secret_token = 'e783b3c08efa6b0337a1c136fa5284c06c20573e55efbdfb619f51adbfc103e6d678173840ccdc7422f0978f1121040f02e782a415747149fce36ba5bf782869'
