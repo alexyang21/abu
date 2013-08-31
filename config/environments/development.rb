f22224ba857dafd56ccfe7c353fea814ebd043dc
Alex Yang <youremail@gmail.com>
2013-08-30 18:03:00 -0700

    add simple_form and devise with new User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Abu::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Used for Devise gem
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
